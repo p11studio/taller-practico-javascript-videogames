5d0e336b35063e634b49a0e3078f38e41b7cca8f
Hugo Ponce <p11studyo@gmail.com>
2023-09-28 14:53:11 -0600

    Clase 3 Elementos del canvas

--- game.js
+++ game.js
@@ -4,12 +4,38 @@ const game = canvas.getContext('2d')
 window.addEventListener('load', startGame)
 
 function startGame() {
+    let canvasSize;
+
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize = window.innerHeight * 0.8;
+    }
+
+    canvas.setAttribute('width', canvasSize)
+    canvas.setAttribute('height', canvasSize)
+
+    const elementSize = canvasSize / 10;
+    console.log({canvasSize, elementSize})
+
+    game.font = elementSize + 'px Verdana';
+    game.textAlign = 'end'
+    
+
+    for (let i = 1; i <= 10; i++) {
+        game.fillText(emojis['X'], elementSize, elementSize * i)   
+    }
+    //window.innerWidth
+
+
+
+
     //game.fillRect(0,0,100,100)
     //game.clearRect(37.5,37.5,25,25)
 
-    game.font= '25px Verdada'
-    game.fillStyle= 'purple'
-    game.textAlign= 'center'
-    game.fillText('Platzi', 25,25)
+    // game.font= '25px Verdada'
+    // game.fillStyle= 'purple'
+    // game.textAlign= 'center'
+    // game.fillText('Platzi', 25,25)
     
 }
